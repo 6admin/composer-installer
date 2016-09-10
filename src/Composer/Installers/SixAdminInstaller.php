57073d84158cd23ca1164df935acc74704374b43
Favre Anael <anael.favre@gmail.com>
2016-09-10 19:59:50 +0200

    Semantic naming compatible PSR-4

--- src/Composer/Installers/SixAdminInstaller.php
+++ src/Composer/Installers/SixAdminInstaller.php
@@ -6,4 +6,29 @@ class SixAdminInstaller extends BaseInstaller
     protected $locations = array(
         'module' => 'modules/{$name}/',
     );
+
+    /**
+     * Format package name.
+     *
+     * For package type six-module, cut off a trailing '-module', convert "-" to "_",
+     * and ucwords the name of the module.
+     *
+     */
+    public function inflectPackageVars($vars)
+    {
+        if ($vars['type'] === 'six-module') {
+            return $this->inflectModuleVars($vars);
+        }
+
+        return $vars;
+    }
+    
+    protected function inflectModuleVars($vars)
+    {
+        $vars['name'] = preg_replace('/-module$/', '', $vars['name']);
+        $vars['name'] = str_replace(array('-', '_'), ' ', $vars['name']);
+        $vars['name'] = str_replace(' ', '', ucwords($vars['name']));
+
+        return $vars;
+    }
 }
